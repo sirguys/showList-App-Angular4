80c80a560257ac2836cf7be0a0fcf10291e7c51f
sarawut kodkeaw <sirguys.apple@gmail.com>
2017-06-06 18:17:22 +0700

    Assignment fungjai angular4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { KeysPipe } from './app.keys';
 
 @NgModule({
   declarations: [
+    KeysPipe,
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
